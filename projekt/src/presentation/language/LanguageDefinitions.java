8d8a7c04ad3c5a308027d6879a1898359f315787
mikkelfrederiksen <mikkelfrederiksen@192.168.0.107>
2017-04-14 22:53:57 +0200

    Created basic ChanceCard definitions

--- projekt/src/presentation/language/LanguageDefinitions.java
+++ projekt/src/presentation/language/LanguageDefinitions.java
@@ -47,4 +47,72 @@ interface LanguageDefinitions {
 	 String bribeJail(String name);
 	 
 	 String threeThrowsJail(String name);
+	 
+	 String ChanceCard1 ();
+	 
+	 String ChanceCard2 ();
+	 
+	 String ChanceCard3 ();
+	 
+	 String ChanceCard4 ();
+	 
+	 String ChanceCard5 ();
+	 
+	 String ChanceCard6 ();
+	 
+	 String ChanceCard7 ();
+	 
+	 String ChanceCard8 ();
+	 
+	 String ChanceCard9 ();
+	 
+	 String ChanceCard10 ();
+	 
+	 String ChanceCard11 ();
+	 
+	 String ChanceCard12 ();
+	 
+	 String ChanceCard13 ();
+	 
+	 String ChanceCard14 ();
+	 
+	 String ChanceCard15 ();
+	 
+	 String ChanceCard16 ();
+	 
+	 String ChanceCard17 ();
+	 
+	 String ChanceCard18 ();
+	 
+	 String ChanceCard19 ();
+	 
+	 String ChanceCard20 ();
+	 
+	 String ChanceCard21 ();
+	 
+	 String ChanceCard22 ();
+	 
+	 String ChanceCard23 ();
+	 
+	 String ChanceCard24 ();
+	 
+	 String ChanceCard25 ();
+	 
+	 String ChanceCard26 ();
+	 
+	 String ChanceCard27 ();
+	 
+	 String ChanceCard28 ();
+	 
+	 String ChanceCard29 ();
+	 
+	 String ChanceCard30 ();
+	 
+	 String ChanceCard31 ();
+	 
+	 String ChanceCard32 ();
+	 
+	 String ChanceCard33 ();
+	 
+	 String ChanceCard34 ();
 }
--- projekt/src/presentation/language/LanguageHandler.java
+++ projekt/src/presentation/language/LanguageHandler.java
@@ -119,5 +119,109 @@ public class LanguageHandler {
 	
 	public String threeThrowsJail(String name){
 		return selectedLanguage.threeThrowsJail(name);
+		
+	public String ChanceCard1 (){
+		return selectedLanguage.ChanceCard1();
+	}
+	
+	public String ChanceCard2 (){
+		return selectedLanguage.ChanceCard2();
+	}
+		
+	public String ChanceCard3 (){
+		return selectedLanguage.ChanceCard3();
+	}
+	public String ChanceCard4 (){
+		return selectedLanguage.ChanceCard4();
+	}
+	public String ChanceCard5 (){
+		return selectedLanguage.ChanceCard5();
+	}
+	public String ChanceCard6 (){
+		return selectedLanguage.ChanceCard6();
+	}
+	public String ChanceCard7 (){
+		return selectedLanguage.ChanceCard7();
+	}
+	public String ChanceCard8 (){
+		return selectedLanguage.ChanceCard8();
+	}
+	public String ChanceCard9 (){
+		return selectedLanguage.ChanceCard9();
+	}
+	public String ChanceCard10 (){
+		return selectedLanguage.ChanceCard10();
+	}
+	public String ChanceCard11 (){
+		return selectedLanguage.ChanceCard11();
+	}
+	public String ChanceCard12 (){
+		return selectedLanguage.ChanceCard12();
+	}
+	public String ChanceCard13 (){
+		return selectedLanguage.ChanceCard13();
+	}
+	public String ChanceCard14 (){
+		return selectedLanguage.ChanceCard14();
+	}
+	public String ChanceCard15 (){
+		return selectedLanguage.ChanceCard15();
+	}
+	public String ChanceCard16 (){
+		return selectedLanguage.ChanceCard16();
+	}
+	public String ChanceCard17 (){
+		return selectedLanguage.ChanceCard17();
+	}
+	public String ChanceCard18 (){
+		return selectedLanguage.ChanceCard18();
+	}
+	public String ChanceCard19 (){
+		return selectedLanguage.ChanceCard19();
+	}
+	public String ChanceCard20 (){
+		return selectedLanguage.ChanceCard20();
+	}
+	public String ChanceCard21 (){
+		return selectedLanguage.ChanceCard21();
+	}
+	public String ChanceCard22 (){
+		return selectedLanguage.ChanceCard22();
+	}
+	public String ChanceCard23 (){
+		return selectedLanguage.ChanceCard23();
+	}
+	public String ChanceCard24 (){
+		return selectedLanguage.ChanceCard24();
+	}
+	public String ChanceCard25 (){
+		return selectedLanguage.ChanceCard25();
+	}
+	public String ChanceCard26 (){
+		return selectedLanguage.ChanceCard26();
+	}
+	public String ChanceCard27 (){
+		return selectedLanguage.ChanceCard27();
+	}
+	public String ChanceCard28 (){
+		return selectedLanguage.ChanceCard28();
+	}
+	public String ChanceCard29 (){
+		return selectedLanguage.ChanceCard29();
+	}
+	public String ChanceCard30 (){
+		return selectedLanguage.ChanceCard30();
+	}
+	public String ChanceCard31 (){
+		return selectedLanguage.ChanceCard31();
+	}
+	public String ChanceCard32 (){
+		return selectedLanguage.ChanceCard32();
+	}
+	public String ChanceCard33 (){
+		return selectedLanguage.ChanceCard33();
+	}
+	public String ChanceCard34 (){
+		return selectedLanguage.ChanceCard34();
 	}
 }
